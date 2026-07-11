820b51fbdb2ff499bf36dd24c29eb83bb127ec65
andrewmanzano <andoromanzano@gmail.com>
2026-07-11 12:32:35 +0800

    GetRandomMeal.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 1: GET Random Meal
@@ -17,10 +19,20 @@ public static class GetRandomMeal
     public static async Task Run(System.Net.Http.HttpClient client)
     {
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/random.php
+        var response = await client.GetAsync(
+           "https://themealdb.com/api/json/v1/1/random.php");
         // TODO: Read the response content as a string
+        string body = await response.Content.ReadAsStringAsync();
         // TODO: Assert status code is 200 OK
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception(
+                $"Expected 200 OK, but got {(int)response.StatusCode}");
+        }
         // TODO: Assert response body is not null or empty
-
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(body))
+        {
+            throw new Exception("Response body is empty.");
+        }
     }
 }
