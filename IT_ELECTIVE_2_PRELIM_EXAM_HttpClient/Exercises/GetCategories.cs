4eead09d5cff3a6e6a8606897f9cf090cfa1e35b
andrewmanzano <andoromanzano@gmail.com>
2026-07-11 12:25:47 +0800

    GetCategories.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 4: GET List Categories
@@ -15,10 +18,23 @@ public static class GetCategories
     public static async Task Run(System.Net.Http.HttpClient client)
     {
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/categories.php
+        var response = await client.GetAsync(
+            "https://themealdb.com/api/json/v1/1/categories.php");
         // TODO: Assert status code is 200 OK
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception(
+                $"Expected 200 OK, but got {(int)response.StatusCode}");
+        }
         // TODO: Parse the response JSON
-        // TODO: Assert the "categories" array has more than 0 items
+        string json = await response.Content.ReadAsStringAsync();
+        using JsonDocument document = JsonDocument.Parse(json);
 
-        throw new NotImplementedException();
+        JsonElement categories = document.RootElement.GetProperty("categories");
+        // TODO: Assert the "categories" array has more than 0 items
+        if (categories.GetArrayLength() <= 0)
+        {
+            throw new Exception("Categories array is empty.");
+        }
     }
 }
